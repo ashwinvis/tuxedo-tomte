f5bf122674be2312e76421c756b0e1df232d8268
E. Mohr <tux@tuxedocomputers.com>
2022-03-04 10:26:32 +0100

    test for login1.Manager

--- test/dbusInhibit.pl
+++ test/dbusInhibit.pl
@@ -1,9 +1,14 @@
 #!/usr/bin/perl
 use Net::DBus;
 my $bus = Net::DBus->session;
-my $svc = $bus->get_service('org.freedesktop.PowerManagement');
-my $obj = $svc->get_object('/org/freedesktop/PowerManagement', 'org.freedesktop.PowerManagement');
-my $id = $obj->Inhibit('appname', 'dialog-information');
+#my $svc = $bus->get_service('org.freedesktop.PowerManagement');
+#my $obj = $svc->get_object('/org/freedesktop/PowerManagement', 'org.freedesktop.PowerManagement');
+#my $id = $obj->Inhibit('appname', 'dialog-information');
+
+my $svc = $bus->get_service('org.freedesktop.login1');
+my $obj = $svc->get_object('org/freedesktop/login1', 'org.freedesktop.login1.Manager');
+my $id = $obj->Inhibit('idle:sleep:shutdown', 'tuxedo-tomte', 'installing fixes', 'block');
+
 
 print "enter: ";
 my $input = <STDIN>;
