f3d2c6328df625765c24384a19eae2a675db465d
E. Mohr <tux@tuxedocomputers.com>
2022-03-09 08:20:06 +0100

    before checkout

--- test/dbusInhibit.pl
+++ test/dbusInhibit.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/perl
 use Net::DBus;
-my $bus = Net::DBus->session;
+my $bus = Net::DBus->system;
+#my $bus = Net::DBus->session;
 #my $svc = $bus->get_service('org.freedesktop.PowerManagement');
 #my $obj = $svc->get_object('/org/freedesktop/PowerManagement', 'org.freedesktop.PowerManagement');
 #my $id = $obj->Inhibit('appname', 'dialog-information');
 
 my $svc = $bus->get_service('org.freedesktop.login1');
+print "before obj\n";
 my $obj = $svc->get_object('org/freedesktop/login1', 'org.freedesktop.login1.Manager');
-my $id = $obj->Inhibit('idle:sleep:shutdown', 'tuxedo-tomte', 'installing fixes', 'block');
+print "before id\n";
+my $id = $obj->Inhibit('shutdown', 'tuxedo-tomte', 'installing fixes', 'block');
 
 
 print "enter: ";
