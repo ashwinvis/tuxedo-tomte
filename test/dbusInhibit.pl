968ea2931c2c794c38b40f3d5a293da3802f2169
E. Mohr <tux@tuxedocomputers.com>
2022-03-11 15:52:40 +0100

    before logout

--- test/dbusInhibit.pl
+++ test/dbusInhibit.pl
@@ -2,17 +2,19 @@
 use Net::DBus;
 use IO::Handle;
 use Scalar::Util qw(openhandle);
-my $bus = Net::DBus->system;
-#my $bus = Net::DBus->session;
-#my $svc = $bus->get_service('org.freedesktop.PowerManagement');
-#my $obj = $svc->get_object('/org/freedesktop/PowerManagement', 'org.freedesktop.PowerManagement');
-#my $id = $obj->Inhibit('appname', 'dialog-information');
+#my $bus = Net::DBus->system;
+my $bus = Net::DBus->session;
+my $svc = $bus->get_service('org.freedesktop.PowerManagement');
+my $obj = $svc->get_object('/org/freedesktop/PowerManagement', 'org.freedesktop.PowerManagement.Inhibit');
+print "hasinhibit: $obj->HasInhibit()\n";
+my $id = $obj->Inhibit('appname', 'dialog-information');
+print "hasinhibit: $obj->HasInhibit()\n";
 
-print "before get_service\n";
-my $svc = $bus->get_service('org.freedesktop.login1');
-print "before obj\n";
-my $obj = $svc->get_object('/org/freedesktop/login1', 'org.freedesktop.login1.Manager');
-print "before id\n";
+#print "before get_service\n";
+#my $svc = $bus->get_service('org.freedesktop.login1');
+#print "before obj\n";
+#my $obj = $svc->get_object('/org/freedesktop/login1', 'org.freedesktop.login1.Manager');
+#print "before id\n";
 
 #foreach (@{$obj->ListUsers}) {
 #	print "$_\n";
@@ -23,15 +25,15 @@ print "before id\n";
 #}
 
 #my $id = new IO::Handle;
-my $id = IO::Handle->new();
-$id = $obj->Inhibit('shutdown:sleep:idle:handle-power-key:handle-suspend-key:handle-hibernate-key:handle-lid-switch', 'tuxedo-tomte', 'installing fixes', 'block');
+#my $id = IO::Handle->new();
+#$id = $obj->Inhibit('shutdown:sleep:idle:handle-power-key:handle-suspend-key:handle-hibernate-key:handle-lid-switch', 'tuxedo-tomte', 'installing fixes', 'block');
 
 #$obj->Reboot(1);
 
-if ($id->opened()) {
-	print "open\n";
-}
+#if ($id->opened()) {
+#	print "open\n";
+#}
 
-print "ID: $id\n";
+#print "ID: $id\n";
 print "enter: ";
 my $input = <STDIN>;
